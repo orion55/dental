6f668d64f49f99ef55eef8303304f1e1a990523a
orion55 <grol55@mail.ru>
2019-03-18 17:09:31 +0500

    add logo

--- header.php
+++ header.php
@@ -4,8 +4,9 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="<?php bloginfo( 'charset' ); ?>" />
     <link rel="profile" href="http://gmpg.org/xfn/11" />
+    <link rel="shortcut Icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/img/favicons/favicon.png" />
     <?php wp_head(); ?>
-</head> 
+</head>
 
 <body <?php body_class(); orion_get_passepartout();?> >
 		<?php get_template_part( 'templates/header/header', 'search' ); ?>
@@ -13,5 +14,5 @@
 		<?php orion_is_boxed('start');?>
 		<?php orion_get_top_bar();?>
 	<div class="site">
-		<?php 
+		<?php
 		orion_get_header();
